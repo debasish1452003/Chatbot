9e744559f27ef95b8130afb2ed796cb24135170d
debasish1452003 <debasishrana1452003@gmail.com>
2025-01-19 21:52:15 +0530

    Frontend Context-api setup

---
+++ frontend/src/Components/shared/CustomizedInput.tsx
@@ -0,0 +1,20 @@
+import  TextField  from "@mui/material"
+
+type Props = {
+    name: string;
+    type: string;
+    label: string;
+};
+
+// const CustomizedInput = (props: Propss) =
+
+
+// const CustomizedInput = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+export default CustomizedInput
